6130cabe90b1083aa12c5052de35e7752e671cff
Bob Mrosky <rmrosk1@lps.umd.edu>
2018-04-16 20:26:13 -0400

    Full path from home to block to user and back

--- grabBlock.m
+++ grabBlock.m
@@ -11,9 +11,11 @@ blocks = [[0.44, 0,   0]; %abcd
           [0.28, 0.1, 0]; %yz .
           [0.2,  0.1, 0]];%!?,'
 
-qb = [deg2rad(100), pi/4, 0, pi/2, 0, pi/4, deg2rad(-80)]; %resting position above blocks
-Pb = cyton.fkine(qb);
-cyton.plot(qb);
+qh = [deg2rad(100), pi/4, 0, pi/2, 0, pi/4, deg2rad(-80)]; %resting position above blocks
+Ph = cyton.fkine(qh);
+qu = [0, -pi/6, 0, -pi/3, 0, -pi/2, pi];
+Pu = cyton.fkine(qu);
+cyton.plot(qh);
 hold on;
 cubePlot(blocks(1,:), blockDim,blockDim,blockDim, 'b');
 cubePlot(blocks(2,:), blockDim,blockDim,blockDim, 'g');
@@ -24,7 +26,7 @@ cubePlot(blocks(6,:), blockDim,blockDim,blockDim, 'y');
 cubePlot(blocks(7,:), blockDim,blockDim,blockDim, 'k');
 cubePlot(blocks(8,:), blockDim,blockDim,blockDim, 'w');
 
-t = 0:0.05:2;
+t = 20;
 
 while 1
     c = input('', 's');
@@ -32,6 +34,7 @@ while 1
     block = 0;
     face = 0;
     
+    %Determine which block and which face the letter is on
     switch uint8(c)
         case num2cell(uint8('a'):uint8('z'))
             block = idivide(uint8(c) - uint8('a'), 4, 'floor') + 1;
@@ -58,8 +61,19 @@ while 1
     disp(block);
     disp(face);
     
-    P = SE3(blocks(block,:) + [bd, bd, 0.1]) * SE3.Ry(pi) * SE3.Rz(-(pi/2)*double(face-1));
-    T = ctraj(Pb, P, length(t));
-    q = cyton.ikine(T, 'q0', qb);
-    cyton.plot(q);
+    %Position 10cm above block, and point +Y axis in direction of letter face
+    Pa = SE3(blocks(block,:) + [bd, bd, 0.1]) * SE3.Ry(pi) * SE3.Rz(-(pi/2)*double(face-1));
+    %Position to grab block
+    Pb = SE3(blocks(block,:) + [bd, bd, blockDim+0.001]) * SE3.Ry(pi) * SE3.Rz(-(pi/2)*double(face-1));
+    
+    T1 = ctraj(Ph, Pa, t);
+    q1 = cyton.ikine(T1, 'q0', qh);
+    T2 = ctraj(Pa, Pb, t/2);
+    q2 = cyton.ikine(T2, 'q0', q1(end,:));
+    T3 = ctraj(Pb, Pa, t/2);
+    q3 = cyton.ikine(T3, 'q0', q2(end,:));
+    T4 = ctraj(Pa, Pu, t);
+    q4 = cyton.ikine(T4, 'q0', q3(end,:));
+    
+    cyton.plot([q1;q2;q3;q4;flipud(q4);flipud(q3);flipud(q2);flipud(q1)]);
 end
